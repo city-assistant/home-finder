0a146b21a3a5b16e6b1b604b85b3d314018fcda0
Jaeung Kim <jaeung949@gmail.com>
2021-04-20 16:50:46 +0900

    forecasting test

--- server/Predict.py
+++ server/Predict.py
@@ -15,44 +15,40 @@ class Predict:
 
     def action(self, injectedDatas):
         df = pd.DataFrame(injectedDatas)
-        cutParameter = 0.75
-        cut = int(len(df) * cutParameter)
 
-        # forForecast = df
-        train = df.iloc[:cut]
-        test = df.iloc[cut:]
-
-        ax = train.plot()
-        test.plot(ax=ax)
+        forForecast = df.iloc[-12:]
+        train = df
 
         sc = MinMaxScaler()
 
-        # forForecast_sc = sc.fit_transform(forForecast)
+        forForecast_sc = sc.fit_transform(forForecast)
         train_sc = sc.fit_transform(train)
-        test_sc = sc.transform(test)
 
-        # forForecast_sc_df = pd.DataFrame(forForecast_sc, columns=['trade_price_idx_value'], index=train.index)
+        print(forForecast_sc)
+        print(train_sc)
+
+        # forForecast_sc_df = pd.DataFrame([forForecast_sc], columns=['trade_price_idx_value'], index=train.index)
         train_sc_df = pd.DataFrame(train_sc, columns=['trade_price_idx_value'], index=train.index)
-        test_sc_df = pd.DataFrame(test_sc, columns=['trade_price_idx_value'], index=test.index)
 
         for s in range(1, 13):
             # forForecast_sc_df['shift_{}'.format(s)] = train_sc_df['trade_price_idx_value'].shift(s)
             train_sc_df['shift_{}'.format(s)] = train_sc_df['trade_price_idx_value'].shift(s)
-            test_sc_df['shift_{}'.format(s)] = test_sc_df['trade_price_idx_value'].shift(s)
 
         X_train = train_sc_df.dropna().drop('trade_price_idx_value', axis=1)
         y_train = train_sc_df.dropna()[['trade_price_idx_value']]
 
-        X_test = test_sc_df.dropna().drop('trade_price_idx_value', axis=1)
-        y_test = test_sc_df.dropna()[['trade_price_idx_value']]
+        # X_forecast = forForecast_sc_df.dropna().drop('trade_price_idx_value', axis=1)
+        # y_forecast = forForecast_sc_df.dropna()[['trade_price_idx_value']]
 
         X_train = X_train.values
-        X_test= X_test.values
         y_train = y_train.values
-        y_test = y_test.values
+
+        # X_forecast = X_forecast.values
+        # y_forecast = y_forecast.values
 
         X_train_t = X_train.reshape(X_train.shape[0], 12, 1)
-        X_test_t = X_test.reshape(X_test.shape[0], 12, 1)
+
+        # X_forecast_t = X_forecast.reshape(X_forecast.shape[0],12,1)
 
         K.clear_session()
     
@@ -69,7 +65,17 @@ class Predict:
 
         maxChange = max(injectedDatas)
 
+        # y_pred = np.ndarray([])
+        print(X_train)
         y_pred = model.predict(X_train_t[-12:], batch_size=32)
-        print(X_train_t)
-        print([v[0] for v in list(y_pred * maxChange)])
+        # test_pred = model.predict(X_forecast_t, batch_size=32)
+
+        # print(test_pred)
+
+        # print(sc.inverse_transform([v[0] for v in list(y_pred * maxChange)]))
         self.result = [v[0] for v in list(y_pred * maxChange)]
+
+        # x_input = np.ndarray(injectedDatas[-12:], dtype=np.float64)
+        # x_input = x_input.reshape((1, 12, 1))
+        # yhat = model.predict(x_input, verbose=0)
+        # print(yhat)
